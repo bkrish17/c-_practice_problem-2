34c557ffdd4db674bcc1b8df9ccb20efde6fe8eb
bkrish17 <krishbharadwaj07@gmail.com>
2025-07-17 07:56:41 +0530

    sum of the first n natural numbers

---
+++ cpp11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,sum=0;
+    cout<<"enter n numbers ";
+    cin>>n;
+    sum=(n*(n+1))/2;
+    cout<<"sum of n numbers "<<sum;
+    
+    return 0;
+}
+
+//this program calculates the sum of the first n natural numbers 
